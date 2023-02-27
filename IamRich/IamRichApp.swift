59c29d79f8c746510a1ea78cdacbf7dfaee4f43f
Sreekanth <sreekanthps28@gmail.com>
2023-02-27 11:40:47 +0000

    Initial Commit

---
+++ IamRich/IamRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamRichApp.swift
+//  IamRich
+//
+//  Created by SREEKANTH PS on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct IamRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
