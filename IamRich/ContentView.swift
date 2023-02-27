29eee507e96cb73ae8b03cd21a4611f4ecf39d8c
Sreekanth <sreekanthps28@gmail.com>
2023-02-27 13:47:19 +0000

    I Am Rich Initial version *Added App iCon *Added Diamond Image *Initial Screen

--- IamRich/ContentView.swift
+++ IamRich/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.teal.ignoresSafeArea()
+            VStack{
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+            }
         }
-        .padding()
     }
 }
 
